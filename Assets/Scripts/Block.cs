b8e382478a80157b411e89e2051aa07e51134174
hogehoge <susumitosusyo@hotmail.com>
2022-02-15 13:46:59 +0900

    ブロックの幅をランダムに生成

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public float minHeight;
+    public float maxHeight;
+
+    public GameObject root;
+    public float bottomBlock;
+
+    void Start()
+    {
+        ChangeHeight();
+    }
+        
+        void ChangeHeight(){
+            //setting for random height
+            float height = Random.Range(minHeight, maxHeight);
+            root.transform.localPosition = new Vector3(0.0f, height, 0.0f);
+        }
+       void ChangeGap()
+    {
+        float offset = Random.Range(bottomBlock, minHeight);
+        root.transform.localPosition = new Vector3(offset, 0.0f, 0.0f);
+    }
+
+        //receiving message change height
+        void OnScrollEnd()
+        {
+            ChangeHeight();
+        ChangeGap();
+        }
+        
+    }
+
